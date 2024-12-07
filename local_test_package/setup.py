d24f8f37fe40e4771104eba73f8e43349e2381e8
jhonnyARM <angelesjhonny5@gmail.com>
2024-12-07 16:57:19 -0500

    aaa

--- local_test_package/setup.py
+++ local_test_package/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 # Obtiene el directorio actual donde está el archivo setup.py
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
-
+#asd
 setup(
     name="local-tests-package",
     version="1.0.0",
